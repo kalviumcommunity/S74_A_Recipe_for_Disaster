1d60c70fec8bffc97c192c977985b89b88bce058
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-03-19 13:15:54 +0530

    Crud operation done, login and signup endpoint created and connected dynamically

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors=require('cors')
+
+// Import Routes
+const foodFailRoutes = require('./routes');
+const router = require('./controllers/user')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,9 +26,9 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error('MongoDB Connection Error:', error));
 db.once('open', () => console.log('Connected to MongoDB'));
 
-// Import Routes
-const foodFailRoutes = require('./routes');
+
 app.use('/api', foodFailRoutes); // Prefix all routes with /api
+app.use('/api/user',router)
 
 // Start the server
 app.listen(PORT, () => {
